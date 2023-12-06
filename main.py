af9daf8f13540378c0d45ced33d48418f354a68a
goldic <goldicstudent@gmail.com>
2023-12-06 23:48:20 +0400

    Editing main.py

--- main.py
+++ main.py
@@ -5,29 +5,37 @@ import requests
 
 import telebot
 
-
-# Importing Conditions 
+# Importing Conditions
 try:
     import config
 except ImportError:
     print("Can not import config file")
     print("Make sure that you copy config.py.template to config.py")
-    exit() 
+    exit()
+
+
+def debug_message(message):
+    if config.Debug:
+        print(message)
 
 
 # debug feature
-print("PROJECT START")
+debug_message("PROJECT START")
 
 # Bot class creating
 with open("TOKEN.txt", 'r') as token_file:
-    BOT = telebot.TeleBot(token_file.read())
+    TOKEN = token_file.read()
+    
+    # Debug message
+    a = requests.get(url=f'https://api.telegram.org/bot{TOKEN}/getUpdates', )
+    debug_message("Can not connect to bot" if "false" in str(a.content) else "Connected to bot")
+    BOT = telebot.TeleBot(TOKEN)
 
 
 # Start message
 @BOT.message_handler(commands=['start'])
 def start_message(message):
     BOT.send_message(message.chat.id, 'Bot started')
-
 
 
 # Results  variables
@@ -83,7 +91,7 @@ def add_page_prefix(url, number):
 
 # Console debug msg
 def print_find_console(product):
-    print(f"FIND {product[0].text}")
+    debug_message(f"FIND {product[0].text}")
     RESULT.append(
         [f"https://biggeek.ru/{product[0].get('href')}",
          product[0].text.strip(),
@@ -110,7 +118,7 @@ def init_bot(message):
     BOT.send_message(message.chat.id, f"НАЧАТ ПОТОК - {SEARCH_THREAD_INDEX}")
 
     # Console debug msg
-    print(f"START THREAD - {SEARCH_THREAD_INDEX}")
+    debug_message(f"START THREAD - {SEARCH_THREAD_INDEX}")
     while True:
         # Parse all urls from arr
         for url in URLS:
@@ -118,8 +126,8 @@ def init_bot(message):
             global SCANED_URLS
 
             # Console debug msg
-            print(f"NEW URL: {url}")
-            print(f"SCANNED URLS: {SCANED_URLS}")
+            debug_message(f"NEW URL: {url}")
+            debug_message(f"SCANNED URLS: {SCANED_URLS}")
 
             # Parsing url pages {range(100000) is example value, it can be anything}
             for urlPageIndex in range(100000):
@@ -179,13 +187,13 @@ def init_bot(message):
                     SEARCH_THREADS[SEARCH_THREAD_INDEX - 1][2] += 1
 
                     # Check if name in product name
-                    if (CONDITION_1[0] in product[0].text.strip()) or (CONDITION_2[0] in product[0].text.strip()) or (
-                            CONDITION_3[0] in product[0].text.strip()):
+                    if (config.CONDITION_1[0] in product[0].text.strip()) or (config.CONDITION_2[0] in product[0].text.strip()) or (
+                            config.CONDITION_3[0] in product[0].text.strip()):
 
                         # check if price equal CONDITION_1 price
                         # ONLY FOR CONDITION WITH SAME PRICE
                         # if conditions price is not equal just copy code near
-                        if product[1].text.strip() == CONDITION_1[1]:
+                        if product[1].text.strip() == config.CONDITION_1[1]:
                             # debug msg
                             print_find_console(product)
 
@@ -201,14 +209,14 @@ def init_bot(message):
 
                         # unstrict find
                         # product[0].text[0:4].lower() != 'игра' is clearing unstrict, but only for default condition
-                        elif convert_price_to_int(CONDITION_1[1]) \
+                        elif convert_price_to_int(config.CONDITION_1[1]) \
                                 <= \
                                 convert_price_to_int(product[1].text) \
                                 and \
                                 product[0].text[0:4].lower() != 'игра':
 
                             # Debug msg
-                            print(f"UN-STRICT-FIND {product[0].text}")
+                            debug_message(f"UN-STRICT-FIND {product[0].text}")
 
                             # Appending product to NON_STRICT_MATCH
                             NON_STRICT_MATCH.append(
@@ -220,7 +228,7 @@ def init_bot(message):
 
         # switch SCANED_URLS to 0 because scaned all urls
         SCANED_URLS = 0
-        print("❗❗❗URLS END❗❗❗")
+        debug_message("❗❗❗URLS END❗❗❗")
 
 
 # TELEGRAM COMMAND PROCESSING SECTION
@@ -257,9 +265,9 @@ def print_total_pages(message):
 
 
 """
- Потоки работают плохо и можно запустить максимум один, 
- если попытаться больше то бот просто перестанет отвечать на команды.
- Поэтому все что с ними связанно можно просто удалить, если есть желание). 
+Threads work poorly and you can run a maximum of one,
+if you try more, the bot will simply stop responding to commands.
+Therefore, everything related to them can be simply deleted if desired).
 """
 
 
@@ -278,7 +286,9 @@ def print_search_thread_index(message):
 
 
 # for non-stoppable script work
-BOT.polling(none_stop=True)
-
+try:
+    BOT.polling(none_stop=True)
+except telebot.apihelper.ApiTelegramException:
+    print("Check the validity of the token in TOKEN.txt")
 # Debug msg
-print("PROJECT END")
+debug_message("PROJECT END")
